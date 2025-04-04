92f7ba845436fe1580667416a2343ce2b3b2d8ab
skalsait <skalsait@dyl.com>
2025-04-04 09:56:02 +0530

    fix http

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const api = axios.create({
-  baseURL: `http://ac65f95b4630741dd8d5b486963215cc-259840844.us-west-2.elb.amazonaws.com/api`,
+  baseURL: `https://ac65f95b4630741dd8d5b486963215cc-259840844.us-west-2.elb.amazonaws.com/api`,
 });
 
 export default api;
