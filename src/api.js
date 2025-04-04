e3c61fbae383ade10c8a4be2af9e6bc41ef75b6c
skalsait <skalsait@dyl.com>
2025-04-04 10:01:42 +0530

    fix error

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-console.log("API URL:", import.meta.env.VITE_API_URL);
+console.log("API URL:", "https://ac65f95b4630741dd8d5b486963215cc-259840844.us-west-2.elb.amazonaws.com");
 
 const api = axios.create({
   baseURL: `https://ac65f95b4630741dd8d5b486963215cc-259840844.us-west-2.elb.amazonaws.com/api`,
